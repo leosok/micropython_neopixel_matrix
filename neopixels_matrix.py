69bf445607607e74b7f0f6861b30e176b57f2bdc
fischteichit@gmx.de <charley>
2023-05-05 04:19:37 +0200

    already quite good!

--- neopixels_matrix.py
+++ neopixels_matrix.py
@@ -4,91 +4,92 @@
 
 import machine
 import neopixel
+import framebuf
 
 class NeoPixelMatrix:
     HORIZONTAL = 0
     VERTICAL = 1
 
-    def __init__(self, pin, width, height):
+    def __init__(self, pin, width, height, direction=HORIZONTAL):
         self.width = width
         self.height = height
         self.pin = machine.Pin(pin, machine.Pin.OUT)
         self.np = neopixel.NeoPixel(self.pin, width * height)
-        self.flip_direction = NeoPixelMatrix.HORIZONTAL
-
-    def set_flip(self, direction):
-        if direction in [NeoPixelMatrix.HORIZONTAL, NeoPixelMatrix.VERTICAL]:
-            self.flip_direction = direction
-        else:
-            raise ValueError("Invalid direction. Use NeoPixelMatrix.HORIZONTAL or NeoPixelMatrix.VERTICAL.")
+        self.fb = framebuf.FrameBuffer(bytearray(width * height * 2), width, height, framebuf.RGB565)
+        self.direction = direction
 
     def _transform_coordinates(self, x, y):
-        if self.flip_direction == NeoPixelMatrix.HORIZONTAL:
+        if self.direction == NeoPixelMatrix.HORIZONTAL:
             x = self.width - 1 - x
-        elif self.flip_direction == NeoPixelMatrix.VERTICAL:
+        elif self.direction == NeoPixelMatrix.VERTICAL:
             y = self.height - 1 - y
         return x, y
 
-    def set_pixel(self, x, y, color):
-        x, y = self._transform_coordinates(x, y)
-        if 0 <= x < self.width and 0 <= y < self.height:
-            index = x + y * self.width
-            self.np[index] = color
+    def _rgb565_to_rgb888(self, rgb565):
+        r = (rgb565 >> 11) & 0x1F
+        g = (rgb565 >> 5) & 0x3F
+        b = rgb565 & 0x1F
+        return (r << 3, g << 2, b << 3)
+
+
+
+    def _update_np_from_fb(self):
+        counter = 0
+        for w in reversed(range(self.width)):
+            if w % 2 == 0:
+                for h in reversed(range(self.height)):
+                    x, y = self._transform_coordinates(w, h)
+                    rgb565 = self.fb.pixel(x, y)
+                    self.np[counter] = self._rgb565_to_rgb888(rgb565)
+                    counter += 1
+            else:
+                for h in range(self.height):
+                    x, y = self._transform_coordinates(w, h)
+                    rgb565 = self.fb.pixel(x, y)
+                    self.np[counter] = self._rgb565_to_rgb888(rgb565)
+                    counter += 1
+
 
     def fill(self, color):
-        self.np.fill(color)
+        r, g, b = color
+        rgb565 = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3)
+        self.fb.fill(rgb565)
 
     def show(self):
+        self._update_np_from_fb()
         self.np.write()
 
     def clear(self):
         self.fill((0, 0, 0))
         self.show()
 
+    def text(self, string, x, y, color):
 
-if __name__ == '__main__':
-    from machine import Pin
-    from time import sleep
-    #from neopixel_matrix import NeoPixelMatrix
+        r, g, b = color
+        rgb565 = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3)
+        self.fb.text(string, x, y, rgb565)
 
-    # Configure the pin number, width, and height
-    DATA_PIN = 23  # GPIO5 on ESP8266, change this to the pin you have connected to the Neopixels
-    WIDTH = 32
-    HEIGHT = 8
-    DIRECTION = NeoPixelMatrix.HORIZONTAL
+    def scroll(self, dx, dy):
+        self.fb.scroll(dx, dy)
+        self.show()
 
-    matrix = NeoPixelMatrix(DATA_PIN, WIDTH, HEIGHT)
 
-    # Fill the entire matrix with a single color
-    matrix.fill((255, 0, 0))  # Red
-    matrix.show()
-    sleep(1)
+class MockNeoPixelMatrix(NeoPixelMatrix):
+    def __init__(self, width, height, direction=NeoPixelMatrix.HORIZONTAL):
+        self.width = width
+        self.height = height
+        self.direction = direction
+        self.fb = framebuf.FrameBuffer(bytearray(width * height * 2), width, height, framebuf.RGB565)
 
-    # Clear the matrix
-    matrix.clear()
-
-    # Set flip direction to horizontal
-    matrix.set_flip(NeoPixelMatrix.HORIZONTAL)
-
-    # Set individual pixels to different colors
-    matrix.set_pixel(0, 0, (255, 0, 0))  # Red
-    matrix.set_pixel(1, 0, (0, 255, 0))  # Green
-    matrix.set_pixel(2, 0, (0, 0, 255))  # Blue
-    matrix.show()
-    sleep(1)
-
-    # Clear the matrix
-    matrix.clear()
-
-    # Set flip direction to vertical
-    matrix.set_flip(NeoPixelMatrix.VERTICAL)
-
-    # Set individual pixels to different colors
-    matrix.set_pixel(0, 0, (255, 0, 0))  # Red
-    matrix.set_pixel(1, 0, (0, 255, 0))  # Green
-    matrix.set_pixel(2, 0, (0, 0, 255))  # Blue
-    matrix.show()
-    sleep(1)
-
-    # Clear the matrix
-    matrix.clear()
+    def show(self):
+        self._update_np_from_fb()
+        for y in range(self.height):
+            row = ''
+            for x in range(self.width):
+                # index = x + y * self.width
+                r, g, b = self._rgb565_to_rgb888(self.fb.pixel(x, y))
+                if r > 127 or g > 127 or b > 127:
+                    row += '#'
+                else:
+                    row += '.'
+            print(row)
